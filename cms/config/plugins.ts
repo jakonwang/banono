ad02098209d4afac2e11e9a9dddb3ad291214efa
jakonwang <429274160@qq.com>
2026-06-12 10:29:59 +0700

    fix(cms): add default email plugin settings

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,5 +1,16 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  email: {
+    config: {
+      provider: 'sendmail',
+      providerOptions: {},
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM', 'no-reply@joesky3c.com'),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'sales@joesky3c.com'),
+      },
+    },
+  },
+});
 
 export default config;
